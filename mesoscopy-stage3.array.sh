3c890be0464049a029354c47db8240e1ee1254f9
Constantinos Eleftheriou <constantinos.eleftheriou@ed.ac.uk>
2026-09-18 16:09:10 +0100

    revise stage 3

--- mesoscopy-stage3.array.sh
+++ mesoscopy-stage3.array.sh
@@ -2,28 +2,29 @@
 
 # To be submitted as a qsub array job
 # Requires a text file with filepaths to process, one path per line
-# E.g. qsub -t 1-$(cat filestoprocess.txt | wc -l) mesoscopy-stage1.array.sh filestoprocess.txt
-# Where filestoprocess.txt contains 20 paths to files that need processing
+# E.g. qsub -t 1-$(cat filestoprocess.txt | wc -l) mesoscopy-stage3.array.sh filestoprocess.txt
+# Where filestoprocess.txt contains paths to files that need processing
+
 
 #$ -N mesoscopystage3
 
-#$ -l h_rt=02:30:00
+#$ -l h_rt=01:30:00
 #$ -l h_rss=4G
-#$ -pe sharedmem 8
+#$ -pe sharedmem 2
 
 #$ -hold_jid stagein
 
-#$ -m eas
-#$ -M Constantinos.Eleftheriou@ed.ac.uk
-
 #$ -o logs/mesoscopystage3_$JOB_ID.log
 #$ -e logs/mesoscopystage3_$JOB_ID.errors
-
 PATH="$PATH:~/.local/bin/"
 OUTBOX="/exports/eddie/scratch/$USER/outbox"
 
 # SGE_TASK_ID will go from 1 to the number of files when we submit an array job
 F=`sed -n ${SGE_TASK_ID}p < $1`
 
-mesoscopy process regions -o "$OUTBOX" "${F}"
+mesoscopy align --behaviour-json "${F/_meso*.h5/.json}" "$F"
+mesoscopy process regions -o "$OUTBOX" "$F"
 
+F_out="${F/inbox/outbox}"
+echo "${F_out/.h5/_regions.csv}" "${F/behaviour-*.json/behaviour-gonogo_trials.csv}"
+mesoscopy process peri-event --post 4.0 --baseline -1 0 --with-metrics -o "$OUTBOX" "${F_out/.h5/_regions.csv}" "${F/behaviour-*.h5/behaviour-gonogo_trials.csv}"
